1491e325a168d6f8fca0f757f317d13f2be24486
Jon Callan <jon@cymbalware.com>
2021-05-07 12:13:15 +0100

    Added matrix divide, add, subtract, exp

--- matrix.c
+++ matrix.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <string.h>
 #include "impulso.h"
 
 /* Macro to get a[i][j] where a has cols columns: can be used as an lvalue */
@@ -8,6 +9,7 @@
  * Multiply an n x m matrix by an m x n matrix
  * z = x * y
  * z is an n x n matrix
+ * z cannot be the same pointer as x or y
  */
 void matrix_multiply(float *z, const float *x, const float *y, int n, int m)
 {
@@ -24,6 +26,63 @@ void matrix_multiply(float *z, const float *x, const float *y, int n, int m)
                 f_tmp += element(x, i, k, m) * element(y, k, j, n);
             }
             element(z, i, j, n) = f_tmp;
+        }
+    }
+}
+
+/*
+ * Divide an n x m matrix by a scalar
+ * y = x / a
+ * where y is an n x m matrix and a is a float
+ * y can be the same pointer as x
+ */
+void matrix_divide(float *y, const float *x, float a, int m, int n)
+{
+    int i, j;
+    
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            element(y, i, j, n) = element(x, i, j, n) / a;
+        }
+    }
+}
+
+/*
+ * Add an n x m matrix to an n x m matrix
+ * z = x + y
+ * z is an n x m matrix
+ * z can be the same pointer as either x or y (or both)
+ */
+void matrix_add(float *z, const float *x, const float *y, int m, int n)
+{
+    int i, j;
+    
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            element(z, i, j, n) = element(x, i, j, n) + element(y, i, j, n);
+        }
+    }
+}
+
+/*
+ * Subtract an n x m matrix from an n x m matrix
+ * z = x - y
+ * z is an n x m matrix
+ * z can be the same pointer as either x or y (or both)
+ */
+void matrix_subtract(float *z, const float *x, const float *y, int m, int n)
+{
+    int i, j;
+    
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            element(z, i, j, n) = element(x, i, j, n) - element(y, i, j, n);
         }
     }
 }
@@ -71,20 +130,20 @@ void matrix_g_elim(float *a, unsigned m, unsigned n)
             }
             /* Do for all rows below pivot: */
             for (i = h + 1; i < m; ++i)
-             {
-                 f_tmp = element(a, i, k, n) / element(a, h, k, n);
-                 /* Fill with zeros the lower part of pivot column: */
-                 element(a, i, k, n) = 0.0f;
-                 /* Do for all remaining elements in current row: */
-                 for (j = k + 1; j < n; ++j)
-                 {
-                     element(a, i, j, n) = element(a, i, j, n) - element(a, h, j, n) * f_tmp;
-                 }
-             }
-             /* Increase pivot row and column */
-             ++h;
-             ++k;
-         }
+            {
+                f_tmp = element(a, i, k, n) / element(a, h, k, n);
+                /* Fill with zeros the lower part of pivot column: */
+                element(a, i, k, n) = 0.0f;
+                /* Do for all remaining elements in current row: */
+                for (j = k + 1; j < n; ++j)
+                {
+                    element(a, i, j, n) = element(a, i, j, n) - element(a, h, j, n) * f_tmp;
+                }
+            }
+            /* Increase pivot row and column */
+            ++h;
+            ++k;
+        }
     }
 }
 
@@ -214,12 +273,44 @@ int matrix_invert(float *y, const float *x, int n, float *ws)
     return 0;
 }
 
+/*
+ * Calculate y = exp(x) where x is an n x n matrix
+ * Requires two workspaces of (n * n) floats
+ * y and x cannot be the same pointer
+ */
+void matrix_exp(float *y, const float *x, int n, int iterations, float *ws1, float *ws2)
+{
+    int i;
+    
+    /* First set y and ws to be the identity matrix In */
+    memset(y, 0, sizeof(float) * n * n);
+    for (i = 0; i < n; ++i)
+    {
+        element(y,   i, i, n) = 1.0f;
+        element(ws1, i, i, n) = 1.0f;
+    }
+    
+    /* For each iteration, add another term in the Taylor series to the answer */
+    for (i = 1; i <= iterations; ++i)
+    {
+        /* We assume ws1 contains the previous term, x^(i-1) / (i-1)!  */
+        matrix_multiply(ws2, ws1, x, n, n);             /*   ws2 = ws1 * x   */
+        matrix_divide(ws1, ws2, (float)i, n, n);        /*   ws1 = ws2 / i   */
+        /* ws1 now contains the current term, x^i / i!   */
+        matrix_add(y, y, ws1, n, n);                    /*   y += ws1        */
+    }
+}
+
+/********************************** Tests ********************************/
+
 float x[7 * 14];
 float y[7 * 7];
-float q[4 * 3] = {1,  9, -1,
+float q[7 * 7] = {1,  9, -1,
                   0,  0,  5,
                   1, -1,  0,
                   0,  3,  2};
+float e[2 * 2] = {1.2, 5.6,
+                  3,   4};
 
 const float z[49] = {-11.5,3.1,7.1,-4.1,5.1,6.1,7.1,
                      9.5,0,0,1,-14,0,14,
@@ -248,4 +339,10 @@ void matrix_test(void)
     matrix_multiply(q, y, z, 7, 7);
     dprintf("\nProduct is:\n");
     matrix_print(q, 7, 7);
+    
+    matrix_exp(q, e, 2, 35, q + 4, q + 8);
+    dprintf("\nExp() of\n");
+    matrix_print(e, 2, 2);
+    dprintf("is:\n");
+    matrix_print(q, 2, 2);
 }
